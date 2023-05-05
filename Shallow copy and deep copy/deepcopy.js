122bd68d68ab39a22dcb9b6f78b7a53245b134a0
RichaBharti <richabharti154@gmail.com>
2023-05-05 09:25:27 +0530

    deep copy and shallowcopy

--- Shallow copy and deep copy/deepcopy.js
+++ Shallow copy and deep copy/deepcopy.js
@@ -2,8 +2,16 @@ let person={
     name:'richa',
     age:23
 }
-let person2=
+let person2=                   // 1 st example
    JSON.parse(JSON.stringify(person))
    person2.age=22
    console.log(person2)
   
+
+   const originalObj = {a: 1, b: {c: 2}};   // 2nd example
+const deepCopyObj = JSON.parse(JSON.stringify(originalObj));
+
+originalObj.b.c = 3;
+
+console.log(originalObj); // {a: 1, b: {c: 3}}
+console.log(deepCopyObj); // {a: 1, b: {c: 2}} - the nested object is a separate object with the same values as in the original object
--- Shallow copy and deep copy/shallow copy.js
+++ Shallow copy and deep copy/shallow copy.js
@@ -1,8 +1,17 @@
 let person={
-   name:'richa',
+   name:'richa',    // first example
    age:23
 }
 let person2=person
 person2.age=24
 console.log(person)
 console.log(person2)
+
+
+const originalArray = [1, 2, {a: 3}];
+const shallowCopyArray = originalArray.slice();  // second example
+
+originalArray[2].a = 4;
+
+console.log(originalArray); // [1, 2, {a: 4}]
+console.log(shallowCopyArray); // [1, 2, {a: 4}] - the nested object is still the same object as in the original array
